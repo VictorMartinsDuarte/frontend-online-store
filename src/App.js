60b3e051e7618777f451d1a838f2e73c10ddf954
Danilo <danilo.santossouza@outlook.com>
2021-09-20 15:56:31 -0300

    Retirando getCategories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import { getCategories } from './services/api';
 
 function App() {
   return (
     <div className="App">
-      {getCategories()}
+      Teste
     </div>
   );
 }
