556ab559bab1e4c8ef46fbbb8b76ec5eaf31ef7a
Victor Martins <victor.martins.duarte@gmail.com>
2021-09-22 18:16:01 -0300

    Resolvidos os conflitos entre branches

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AddToCartButton from './AddToCartButton';
 
 class ProductCard extends React.Component {
   render() {
-    const { arrayProduct: { title, price, thumbnail } } = this.props;
+    const { arrayProduct } = this.props;
     const product = {
-      title,
-      price,
-      thumbnail,
+      title: arrayProduct.title,
+      price: arrayProduct.price,
+      thumbnail: arrayProduct.thumbnail,
     };
     return (
-      <div data-testid="product">
-        <h4>{title}</h4>
-        <img src={ thumbnail } alt={ title } />
-        <span>{ `Preço: R$ ${price}` }</span>
-        <AddToCartButton arrayProduct={ product } />
+      <div>
+        <div data-testid="product">
+          <h4>{arrayProduct.title}</h4>
+          <img src={ arrayProduct.thumbnail } alt={ arrayProduct.title } />
+          <span>{ `Preço: R$ ${arrayProduct.price}` }</span>
+          <AddToCartButton arrayProduct={ product } />
+        </div>
+        <div>
+          <Link
+            data-testid="product-detail-link"
+            to={ `/product-detail/${arrayProduct.id}/${arrayProduct.category_id}` }
+          >
+            <button type="button">mais detalhes</button>
+          </Link>
+        </div>
       </div>
     );
   }
 }
 
 ProductCard.propTypes = {
-  arrayProduct: PropTypes.arrayOf(
-    PropTypes.object,
-  ).isRequired,
-};
+  arrayProduct: PropTypes.array,
+}.isRequired;
 
 export default ProductCard;
