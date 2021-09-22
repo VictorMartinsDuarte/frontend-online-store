32954543235b7a02347c6f1880b84da1e8fee1e5
Victor Martins <victor.martins.duarte@gmail.com>
2021-09-22 17:26:09 -0300

    Ajustando a questao 3, e 8 que estavam conflitando

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 class ShoppingCart extends React.Component {
   render() {
     const cartItems = JSON.parse(localStorage.getItem('shoppingCart'));
+    if (cartItems === '' || cartItems === null) {
+      return (
+        <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
+      );
+    }
     return (
       <div>
-        <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
         <h4 data-testid="shopping-cart-product-name">{ cartItems.title }</h4>
         <span data-testid="shopping-cart-product-quantity">{cartItems.count}</span>
         {/* <span>{ `Preço: R$ ${cartItems.price}` }</span> */}
